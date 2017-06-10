35177a7929b8e17168d06107598300389893bb00
Sam Light <samlight1994@gmail.com>
2017-06-10 08:33:25 +0100

    Work on layout and display of editor.

--- src/wp_grid_editor/editor.cljs
+++ src/wp_grid_editor/editor.cljs
@@ -5,53 +5,62 @@
   (:require
    [hoplon.core :as h]
    [javelin.core :as j]
-   [wp-grid-editor.editor.state :as s]))
-
-(h/defelem menu-bar-main []
-  (h/div
-   :class "lsgc_main_bar"
-   ))
-
-(h/defelem menu [_ items]
-  (h/div
-   :class "lsgc_menu"
-   items))
+   [clojure.string :as string]
+   [wp-grid-editor.editor.state :as s]
+   [wp-grid-editor.editor.menus :as menus]))
 
 (h/defelem self-closing-shortcode [{:keys [elem]}]
   (j/cell-let
-   [{:keys [parameters] n :name} elem]
-   [(menu)
-    (h/h3 n)
+   [{:keys [parameters key] n :name} elem]
+   [(menus/modual)
+    (h/h3 (j/cell= (or n key)))
     (h/ul
      (h/loop-tpl
       :bindings [[k v] (j/cell= (vec parameters))]
-      (h/li (j/cell= (str (name k) ": " v)))))]))
+      (h/li (j/cell= (str (name key) ": " v)))))]))
 
 (h/defelem shortcode [{:keys [content elem]}]
   (j/cell-let
-   [{:keys [name]} elem]
-   [(menu)
-    (h/h3 name)
-    (content)]))
+   [{:keys [key tile_classes] n :name} elem]
+   (h/div
+    :class tile_classes
+    (menus/modual)
+    (h/h3 (j/cell= (or n key)))
+    (content))))
 
 (h/defelem content [{{:keys [content]} :elem}]
-  [(menu)
+  [(menus/modual)
    (h/div content)])
+
+(defn map-display [elems display]
+  (when (and (-> elems empty? not)
+             (-> display empty? not))
+    (vec (map
+          (fn [{:keys [key nodes element self-closing] :as e}]
+            (let [r (if (-> nodes empty?)
+                      e (assoc e :nodes (map-display nodes display)))]
+              (if (= element :shortcode)
+                (merge r (get display key))
+                r)))
+          elems))))
 
 (h/defelem render [{:keys [elems]}]
   (h/div
    (h/loop-tpl
-    :bindings [{:keys [type self-closing nodes] :as elem} elems]
+    :bindings [{:keys [element self-closing nodes] :as elem} elems]
     (h/div
      (h/cond-tpl
-      (j/cell= (and (= type :shortcode) self-closing))
+      (j/cell= (and (= element :shortcode) self-closing))
       (self-closing-shortcode :elem elem)
-      (j/cell= (and (= type :shortcode) (not self-closing)))
+      (j/cell= (and (= element :shortcode) (not self-closing)))
       (shortcode :elem elem :content (render :elems nodes))
-      (j/cell= (= type :content))
+      (j/cell= (= element :content))
       (content :elem elem))))))
 
 (h/defelem editor []
   (h/div
    :class "lsgc_editor_wrap"
-   (render :elems s/cmain)))
+   (menus/master)
+   (render :elems (j/cell= (map-display s/main s/display-mods-map)))))
+
+(j/cell= (.log js/console (clj->js (map-display s/main s/display-mods-map))))
