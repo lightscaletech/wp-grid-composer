7bebf63149e99720ebeccbd2616aedb3ae1c7040
Sam Light <samlight1994@gmail.com>
2017-06-13 11:59:48 +0100

    Hide wordpress editor when using grid composer.

--- php/Editor.php
+++ php/Editor.php
@@ -10,10 +10,22 @@ class LSGC_Editor {
     }
 
     function before_editor() {
+        global $current_screen;
+
         $this->resources->enqueue_style_grid();
         $this->resources->enqueue_style_editor();
         $this->resources->enqueue_js_editor();
 
+        $cpt = $current_screen->post_type;
+        $ptopts = get_option('post_types');
+        $ptopts = isset($ptopts) ? $ptopts : array();
+
+        $enabled = isset($ptopts[$cpt]) ? $ptopts[$cpt] : FALSE;
+
+        echo "<script>_lsgc_editor_enabled__ = {$enabled};</script>";
+        if ($enabled) {
+            echo '<style> #postdivrich {visibility: collapse; height: 0px;}</style>';
+        }
         echo '<div id="lsgc_editor_container"></div>';
     }
 }
