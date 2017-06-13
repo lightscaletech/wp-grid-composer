294af0af91fa8bb3ec7ceb9788260b3b8ceb2673
Sam Light <samlight1994@gmail.com>
2017-06-13 11:58:33 +0100

    Improvement to parsing the wordpress content to clojure map.

--- src/wp_grid_editor/editor/state.cljs
+++ src/wp_grid_editor/editor/state.cljs
@@ -6,16 +6,18 @@
    [clojure.string :as s]
    [wp-grid-editor.ajax :as ajax]))
 
-(j/defc main "")
+(j/defc main [])
+
+(j/cell= (.log js/console (clj->js main)))
 
 ;; Convert text content to clojure and back
-(def sc-regx #"\[([\w\_\-]+)\ ?((?:[\w\_\-\=\"]+\ ?)*)(/\]|\])(.*)")
+(def sc-regx #"\[([\w\_\-]+)\ ?([\w\_\-\=\"\.\ ]*)(\]|/\])([\w\W\n]*)")
 
 (defn sc-end-regx [n]
   (re-pattern
-   (str "(?:(\\[" n "\\ ?[\\w\\=\\\"\\-\\_]*\\])|(\\[\\/" n "\\]))(.*)")))
+   (str "(?:(\\[" n "\\ ?[\\w\\=\\\"\\-\\_]*\\])|(\\[\\/" n "\\]))([\\w\\W\\n]*)")))
 
-(defn nestr [s] (when (-> s empty? not) s))
+(defn nestr [s] (when (-> s s/blank? not) s))
 
 (defn shortcode-params [ps]
   (reduce
@@ -23,23 +25,30 @@
       (assoc %1 (keyword k) (s/replace v "\"" "")))
    {} (s/split ps #" ")))
 
-(defn front-content [c fm] (nestr (subs c 0 (- (.-length c) (.-length fm)))))
+(defn front-content [c fm]
+  (let [cl (.-length c)
+        fml (.-length fm)]
+    (if (= cl fml)
+      nil
+      (nestr (subs c 0 (- cl fml))))))
 
 (defn shortcode-contents [n c]
-  (if (empty? n)
-    [nil nil]
+  (if (s/blank? n)
+    [nil c]
     (let [regx (sc-end-regx n)]
       (loop [r "" count 0 restc c]
         (let [rer (re-find (sc-end-regx n) restc)
-              [fm osc csc bc] rer
-              fr (front-content restc fm)
-              r' (str r fr osc)]
-          (if (-> osc empty? not)
-            (recur r' (inc count) bc)
-            (if (-> csc empty? not)
-              (if (zero? count)
-                [r' bc]
-                (recur (str r' csc) (dec count) bc)))))))))
+              [fm osc csc bc] rer]
+          (if (s/blank? fm)
+            [r restc]
+            (let [fr (front-content restc fm)
+                  r' (str r fr osc)]
+              (if (-> osc s/blank? not)
+                (recur r' (inc count) bc)
+                (if (-> csc s/blank? not)
+                  (if (zero? count)
+                    [r' bc]
+                    (recur (str r' csc) (dec count) bc)))))))))))
 
 (defn make-content-node [c] {:element :content :content c})
 
@@ -50,19 +59,21 @@
     (if selfc n (assoc n :nodes sc))))
 
 (defn map-shortcodes [c]
-  (when (-> c empty? not)
+  (when (-> c s/blank? not)
     (loop [r [] restc c]
       (let [[fm sn sp endt sr] (re-find sc-regx restc)]
-        (if (empty? fm)
-          (if (empty? restc) r (conj r (make-content-node restc)))
+        (if (s/blank? fm)
+          (if (s/blank? restc) r (conj r (make-content-node restc)))
           (if-let [fr (front-content restc fm)]
-            (recur (conj r (make-content-node fr)) fm)
-            (let [[sc restsc] (if (= endt "]") (shortcode-contents sn sr))
+            (let [r' (conj r (make-content-node fr))] (recur r' fm))
+            (let [[sc restsc] (if (= endt "]")
+                                (shortcode-contents sn sr)
+                                [nil sr])
                   mksc! (partial make-sc-node sn sp)
                   r' (conj r (condp = endt
                                "]"  (mksc! (map-shortcodes sc) false)
                                "/]" (mksc! nil true)))]
-              (if (empty? restsc)
+              (if (s/blank? restsc)
                 r'
                 (recur r' restsc)))))))))
 
@@ -104,7 +115,10 @@
              {:keys @ushortcodes} display-mods))
 
 (defn enable! []
-  (reset! main (map-shortcodes (get-wp-editor-content)))
+  (let [perf (.now js/performance)
+        v (map-shortcodes (get-wp-editor-content))]
+    (.log js/console (str "Whole conversion: " (- (.now js/performance) perf)))
+    (reset! main v))
   (load-display-modules))
 
 (defn disable! [] #_(set-wp-editor-content @main))
